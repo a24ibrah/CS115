7a4ed243667581252d31d359fb4e669ffc5378d7
afibrahim <a24ibrah@uwaterloo.ca>
2015-11-13 10:55:07 -0500

    Initiate repo
    For CS115 codes

---
+++ M08/count-value.rkt
@@ -0,0 +1,43 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname count-value) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+
+
+;; The complete design recipe is left as an exercise.
+
+(define-struct node (key val left right))
+;; A binary search tree (BST) is either
+;; * empty, or
+;; * (make-node Num Str BST BST),
+
+;; which satisfies the ordering property recursively:
+;; * every key in left is less than key
+;; * every key in right is greater than key
+
+;; my-bst-fun: BST → Any
+;; (define (my-bst-fun t)
+;;   (cond
+;;   [(empty? t) . . . ]
+;;   [else . . . (node-key t) . . .
+;;       . . . (node-val t) . . .
+;;       . . . (my-bst-fun (node-left t)) . . .
+;;       . . . (my-bst-fun (node-right t)) . . . ]))
+
+;; (count-values abst v) counting values equal to v    
+;; count-values: BST Str → Nat
+;; Example:
+(check-expect (count-values (make-node 6 "A" (make-node 4 "A" empty empty) (make-node 8 "A" empty empty)) "A") 3)
+(check-expect (count-values 
+(make-node 10 "E"
+(make-node 6 "A" (make-node 4 "D" empty empty) (make-node 8 "C" empty empty))
+(make-node 18 "A" (make-node 15 "A" empty empty) (make-node 21 "A" empty empty))) "A") 4)
+
+(define (count-values abst v)
+  (cond
+    [(empty? abst) 0]
+  [else  
+    (+ (cond
+         [(equal? v (node-val abst)) 1]
+         [else 0])
+       (count-values (node-left abst) v)
+       (count-values (node-right abst) v))]))
---
+++ M08/eval.rkt
@@ -0,0 +1,48 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname eval) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+
+;; The complete design recipe is left as an exercise.
+
+(define-struct binode (op arg1 arg2))
+;;  A Binary arithmetic expression Internal Node (BINode)
+;;  is a (make-binode (anyof  ’*  ’+  ’/  ’-) BinExp BinExp)
+;;  A Binary arithmetic expression (BinExp) is one of:
+;;  * a Num
+;;  * a BINode
+
+;; my-binexp-fun: BinExp → Any
+;; (define (my-binexp-fun ex)
+;;   (cond
+;;   [(number? ex) . . . ]
+;;   [else . . . (binode-opex) . . .
+;;         . . . (my-binexp-fun (binode-arg1 ex)) . . .
+;;         . . . (my-binexp-fun (binode-arg2 ex)) . . . ]))
+
+;; (eval ex) produces the value of a given binary arithmetic expression
+;; eval: BinExp → Num
+;; Examples
+;(check-expect (eval 5) 5) 
+(check-expect (eval (make-binode '* 2 6)) 12)
+;(check-expect (eval (make-binode '+ 2 (make-binode '- 5 3))) 4)
+;(check-expect (eval (make-binode '/
+;                                 (make-binode '+ (make-binode '* 2 6)
+;                                                 (make-binode '* 5 2))
+;                                 (make-binode '- 5 3))) 11)
+
+(define (eval ex)
+  (cond
+    [(number? ex) ex]
+    [else
+     (cond
+      [(symbol=? (binode-op ex) '*)
+        (* (eval (binode-arg1 ex)) (eval (binode-arg2 ex)))]
+      [(symbol=? (binode-op ex) '+)
+        (+ (eval (binode-arg1 ex)) (eval (binode-arg2 ex)))]
+      [(symbol=? (binode-op ex) '/)
+        (/ (eval (binode-arg1 ex)) (eval (binode-arg2 ex)))]
+      [(symbol=? (binode-op ex) '-)
+        (- (eval (binode-arg1 ex)) (eval (binode-arg2 ex)))])
+     ]))
+
+ 
